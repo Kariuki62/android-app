7b9adf9bf3a5427893d5df90ebeb4b28e9a404bd
Kariuki62 <kariukiwan62@gmail.com>
2019-09-02 11:26:29 -0400

    Made changes to the intro class

--- app/src/main/java/com/example/newreads/ui/intro.java
+++ app/src/main/java/com/example/newreads/ui/intro.java
@@ -1,13 +1,14 @@
-package com.example.newreads;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.newreads.ui;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newreads.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
