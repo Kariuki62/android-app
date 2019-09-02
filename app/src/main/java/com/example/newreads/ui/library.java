1a1a72b9988648c044eef67ca0fce936ea1db073
Kariuki62 <kariukiwan62@gmail.com>
2019-09-02 11:34:09 -0400

    Added the library class and all the changes required.

---
+++ app/src/main/java/com/example/newreads/ui/library.java
@@ -0,0 +1,77 @@
+package com.example.newreads.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newreads.R;
+import com.example.newreads.adapter.LibraryListAdapter;
+import com.example.newreads.models.Bookss;
+import com.example.newreads.service.ReadService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class library extends AppCompatActivity {
+    public static final String TAG = library.class.getSimpleName();
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private LibraryListAdapter mAdapter;
+    private ArrayList<Bookss> books = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+        ButterKnife.bind(this);
+        String title = "the monk";
+        getTitle(title);
+
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("library");
+
+    }
+    private void getTitle(String title){
+            final ReadService readService = new ReadService();
+            readService.findTitle(title, new Callback() {
+
+                @Override
+                public void onFailure(okhttp3.Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) {
+                    books = readService.processResults(response);
+                    library.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mAdapter = new LibraryListAdapter(getApplicationContext(), books);
+                            mRecyclerView.setAdapter(mAdapter);
+                            RecyclerView.LayoutManager layoutManager =
+                                    new LinearLayoutManager(library.this);
+                            mRecyclerView.setLayoutManager(layoutManager);
+                            mRecyclerView.setHasFixedSize(true);
+
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+
+
+
