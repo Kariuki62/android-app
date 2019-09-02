3ac6f3cdbfe520debdfa5ac0a65a962d597c8252
Kariuki62 <kariukiwan62@gmail.com>
2019-09-02 11:33:33 -0400

    Added the Constants class and all the changes required.

---
+++ app/src/main/java/com/example/newreads/ui/Constants.java
@@ -0,0 +1,11 @@
+package com.example.newreads.ui;
+
+import com.example.newreads.BuildConfig;
+
+public class Constants {
+    public static final String GOODREADS_TOKEN = BuildConfig.GoodReads_Token;
+    public static final String GOODREADS_BASE_URL = "https://www.goodreads.com/search/index.xml";
+    public static final String GOODREADS_BOOKS_QUERY_PARAMETER = "q";
+    public static final String GOODREADS_KEY_PARAMETER = "key";
+
+}
